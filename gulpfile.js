74c9e6e1cb6b3ff07896c290296ee2e2dec3313a
BeaMorim <biadmorim@gmail.com>
2018-01-10 15:49:43 -0200

    Criação da função para rodar o browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var sequence = require('gulp-sequence');
 var clean = require('gulp-clean');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
+var browserSync = require('browser-sync').create();
 
 
 gulp.task('clean', function () {
@@ -10,15 +11,25 @@ gulp.task('clean', function () {
        .pipe(clean());
 });
 
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./build"
+        }
+    });
+});
+
 gulp.task('html', function() {
     return gulp.src('src/**/*.html')
         .pipe(gulp.dest('build'))
+        .pipe(browserSync.stream());
 })
 
 gulp.task('sass', function() {
     return gulp.src('src/scss/*.scss')
         .pipe(sass())
         .pipe(gulp.dest('build/css'))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('jsConcat', function() {
@@ -28,9 +39,11 @@ gulp.task('jsConcat', function() {
 })
 
 gulp.task('build', function(cb) {
-    sequence('clean', ['html', 'sass', 'jsConcat'])(cb)
+    sequence('clean', ['html', 'sass', 'jsConcat'], 'browserSync')(cb)
 })
 
 gulp.task('default', ['build'], function(cb) {
-    gulp.watch('src/scss/*.scss', ['sass'])
+    gulp.watch('src/scss/*.scss', ['sass']);
+    gulp.watch('src/**/*.html', ['html']);
+    gulp.watch('src/app/**/*.js', ['jsConcat']);
 });
