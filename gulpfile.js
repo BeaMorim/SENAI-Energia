2d30a53fe3c876ef0bc24d96054632738926e734
BeaMorim <biadmorim@gmail.com>
2018-01-10 14:44:41 -0200

    Criação da função para concatenar o js

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,30 @@ var gulp = require('gulp');
 var sequence = require('gulp-sequence');
 var clean = require('gulp-clean');
 var sass = require('gulp-sass');
+var concat = require('gulp-concat');
 
 
-gulp.task('clean-build', function () {
+gulp.task('clean', function () {
    return gulp.src('build', {read: false})
        .pipe(clean());
 });
 
 gulp.task('sass', function() {
     return gulp.src('src/scss/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('build/css'))
+        .pipe(sass())
+        .pipe(gulp.dest('build/css'))
 });
 
-gulp.task('default', function(cb) {
-    sequence('clean-build', 'sass')(cb)
+gulp.task('jsConcat', function() {
+    return gulp.src('src/app/**/*.js')
+        .pipe(concat('scripts.js'))
+        .pipe(gulp.dest('build/app'))
+})
+
+gulp.task('build', function(cb) {
+    sequence('clean', ['sass', 'jsConcat'])(cb)
+})
+
+gulp.task('default', ['build'], function(cb) {
+    gulp.watch('src/scss/*.scss', ['sass'])
 });
