84344dd712e7f138e7add08204feb4f674652fc6
BeaMorim <biadmorim@gmail.com>
2018-01-10 16:05:16 -0200

    Criação da função para otimizar imagens

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var clean = require('gulp-clean');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var browserSync = require('browser-sync').create();
+var imagemin = require('gulp-imagemin');
 
 
 gulp.task('clean', function () {
@@ -36,11 +37,17 @@ gulp.task('jsConcat', function() {
     return gulp.src('src/app/**/*.js')
         .pipe(concat('scripts.js'))
         .pipe(gulp.dest('build/app'))
-})
+});
+
+gulp.task('imagemin', function() {
+    return gulp.src('src/images/**/*.+(png|jpg|svg)')
+        .pipe(imagemin())
+        .pipe(gulp.dest('build/images'))
+});
 
 gulp.task('build', function(cb) {
-    sequence('clean', ['html', 'sass', 'jsConcat'], 'browserSync')(cb)
-})
+    sequence('clean', ['html', 'sass', 'jsConcat', 'imagemin'], 'browserSync')(cb)
+});
 
 gulp.task('default', ['build'], function(cb) {
     gulp.watch('src/scss/*.scss', ['sass']);
